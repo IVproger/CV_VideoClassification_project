2650156e583e9a8c526878efa8f20c11b05a328b
Ivan Golov <ivangolov@icloud.com>
2024-11-12 15:55:03 +0300

    remove old files

--- src/labels.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-ucf50_path = Path(__file__).parent / "raw" / "UCF50"
-idx2cls = {
-    idx: class_dir for idx, class_dir in enumerate(sorted(os.listdir(ucf50_path)))
-}
-cls2idx = {v: k for k, v in idx2cls.items()}
